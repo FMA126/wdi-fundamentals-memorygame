72f7aba09d7ef433825f52584426353ad186b69c
Merritt <mhblanks@gmail.com>
2019-03-23 01:40:19 -0600

    added awesome game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,25 +20,57 @@ const cards = [
         cardImage: "images/king-of-diamonds.png"
     }
 ];
-const cardsInPlay = [];
+let cardsInPlay = [];
+let score = 0;
 
-const checkForMatch = function() {
+const checkForMatch = function(x, elem) {
+    elem.setAttribute('src', cards[x].cardImage)
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
-            console.log("You found a match!");
+            score++;
+            document.getElementById('outcome-score').getElementsByTagName('p')[1].innerHTML = ("Your Score: " + score);
+            document.getElementById('outcome-score').getElementsByTagName('p')[0].innerHTML = ("You found a match!");
         } else {
-            console.log("Sorry try again.");
+            document.getElementById('outcome-score').getElementsByTagName('p')[0].innerHTML = ("Sorry try again");
+            
         }
     }
+    
 }
-const flipCard = function(cardId) {
-    console.log("User flipped " + " " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-
+const flipCard = function() {
+    let cardId = this.getAttribute('data-id');
+    let el = this;
+    
     cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();
+    checkForMatch(cardId, el);
 }
 
-flipCard(0);
-flipCard(2);
+const newGame = function() {
+    cardsInPlay = [];
+    document.getElementById('game-board').innerHTML = '';
+    document.getElementById('outcome-score').getElementsByTagName('p')[0].innerHTML = "You can do it!";
+
+
+    
+    createBoard();
+}
+
+const createBoard = function() {    
+    for (let i = 0; i < cards.length; i++) {
+        let cardElement = document.createElement('img');
+
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+
+    let resetCards = document.createElement('button');
+
+    resetCards.innerHTML = 'Try Again';
+    resetCards.addEventListener('click', newGame);
+    document.getElementById('game-board').appendChild(resetCards);
+
+}
+
+createBoard();
